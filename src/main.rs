d06876f891bd05b5fe0b68d4f31dcb09b5421682
VinWare <28603906+VinWare@users.noreply.github.com>
2024-01-01 15:31:12 -0800

    Single-threaded HTTP server works

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-use std::net::TcpListener;
+use std::{fs, thread};
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+    listener.incoming().for_each(|stream| handle_connection(stream.unwrap()));
+}
 
-        println!("Connection established!");
-    }
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, file_name)  =   match &request_line[..] {
+        "GET / HTTP/1.1" =>      ("HTTP/1.1 200 OK","hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK","hello.html")
+        },
+        _=>
+            ("HTTP/1.1 404 NOT FOUND","404.html")
+    };
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+        //map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
 }
